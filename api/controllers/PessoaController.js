e8e64d514cfd78ddcc5059cfdfb701131a7a1edf
João Lourenço <joaodslourenco@gmail.com>
2022-07-07 09:44:02 -0300

    feat: updatePerson method and route

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -33,6 +33,26 @@ class PessoaController {
       return res.status(500).json(error.message);
     }
   }
+
+  static async updatePerson(req, res) {
+    const { id } = req.params;
+    const newInfo = req.body;
+    try {
+      await database.Pessoas.update(newInfo, {
+        where: {
+          id: Number(id),
+        },
+      });
+      const updatedPerson = await database.Pessoas.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+      return res.status(200).json(updatedPerson);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoutes.js
+++ api/routes/pessoasRoutes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get("/pessoas", PessoaController.getAllPeople);
 router.get("/pessoas/:id", PessoaController.getPersonById);
 router.post("/pessoas", PessoaController.addPerson);
+router.put("/pessoas/:id", PessoaController.updatePerson);
 
 module.exports = router;

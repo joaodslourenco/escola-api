a9f3f795f008cd53b06a472937766be974ad8a37
João Lourenço <joaodslourenco@gmail.com>
2022-07-07 15:27:11 -0300

    fix: added new routes to routes index

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const pessoasRoutes = require("./pessoasRoutes");
+const turmasRoutes = require("./turmasRoutes");
+const niveisRoutes = require("./niveisRoutes");
 
 module.exports = (app) => {
-  app.use(express.json(), pessoasRoutes);
+  app.use(express.json(), pessoasRoutes, turmasRoutes, niveisRoutes);
   app.get("/", (req, res) => {
     res.status(200).send("Olá");
   });

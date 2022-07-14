9d98393ec7fef4fdbc54908433a9606936193211
João Lourenço <joaodslourenco@gmail.com>
2022-07-14 10:27:18 -0300

    feat: validation to nome and email fields

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -18,9 +18,23 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoas.init(
     {
-      nome: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          funcaoValidadora: function (dado) {
+            if (dado.length < 3) {
+              throw new Error("O campo nome deve ter mais de 3 caracteres");
+            }
+          },
+        },
+      },
       ativo: DataTypes.BOOLEAN,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: { args: true, msg: "Dados do tipo e-mail inválidos." },
+        },
+      },
       role: DataTypes.STRING,
     },
     {

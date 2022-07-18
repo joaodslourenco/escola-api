565687f68d4974f5a8fd9bc37319de0af91b20c6
João Lourenço <joaodslourenco@gmail.com>
2022-07-18 11:27:01 -0300

    style: pessoasRoutes grouped by http method

--- api/routes/pessoasRoutes.js
+++ api/routes/pessoasRoutes.js
@@ -6,18 +6,11 @@ const router = Router();
 router.get("/pessoas", PessoaController.getAllActivePeople);
 router.get("/pessoas/todos", PessoaController.getAllPeople);
 router.get("/pessoas/:id", PessoaController.getPersonById);
-router.post("/pessoas", PessoaController.addPerson);
-router.put("/pessoas/:id", PessoaController.updatePerson);
-router.delete("/pessoas/:id", PessoaController.deletePerson);
-router.post("/pessoas/:id/restaura", PessoaController.restorePerson);
-router.post("/pessoas/:estudanteId/cancela", PessoaController.cancelPerson);
-
 router.get(
   "/pessoas/matricula/:turmaId/confirmadas",
   PessoaController.getRegistrationsByClass,
 );
 router.get("/pessoas/matricula/lotada", PessoaController.getFullClasses);
-
 router.get(
   "/pessoas/:estudanteId/matricula",
   PessoaController.getRegistrations,
@@ -26,14 +19,22 @@ router.get(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.getRegistrationById,
 );
+
+router.post("/pessoas", PessoaController.addPerson);
 router.post(
   "/pessoas/:estudanteId/matricula",
   PessoaController.addRegistration,
 );
+router.post("/pessoas/:id/restaura", PessoaController.restorePerson);
+router.post("/pessoas/:estudanteId/cancela", PessoaController.cancelPerson);
+
+router.put("/pessoas/:id", PessoaController.updatePerson);
 router.put(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.updateRegistration,
 );
+
+router.delete("/pessoas/:id", PessoaController.deletePerson);
 router.delete(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.deleteRegistration,

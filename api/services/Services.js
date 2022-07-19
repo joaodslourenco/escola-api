33925a40380faed621cafce260dd22b3b59e5a54
João Lourenço <joaodslourenco@gmail.com>
2022-07-19 12:02:51 -0300

    feat: base services methods

--- api/services/Services.js
+++ api/services/Services.js
@@ -5,17 +5,55 @@ class Services {
     this.modelName = modelName;
   }
 
-  async getAllRecords() {
+  async getAllRecords(where = {}) {
     return database[this.modelName].findAll();
   }
 
-  async getOneRecord(id) {}
+  async getOneRecord(where = {}) {
+    return database[this.modelName].findOne({ where: { ...where } });
+  }
 
-  async createRecord(data) {}
+  async createRecord(data) {
+    return database[this.modelName].create(data);
+  }
 
-  async updateRecord(updatedData, id) {}
+  async updateRecord(updatedData, where, transacao = {}) {
+    return database[this.modelName].update(
+      updatedData,
+      { where: { ...where } },
+      transacao,
+    );
+  }
 
-  async deleteRecord(id) {}
+  async updateRecords(updatedData, where, transacao = {}) {
+    return database[this.modelName].update(
+      updatedData,
+      { where: { ...where } },
+      transacao,
+    );
+  }
+
+  async deleteRecord(id) {
+    return database[this.modelName].destroy({ where: { id: id } });
+  }
+
+  async restoreRecord(id) {
+    return database[this.modelName].restore({ where: { id: id } });
+  }
+
+  async verifyDeletedRecord(id) {
+    return database[this.modelName].findOne({
+      paranoid: false,
+      where: { id: Number(id) },
+    });
+  }
+
+  async findAndCountRecords(where = {}, aggregators) {
+    return database[this.modelName].findAndCountAll({
+      where: { ...where },
+      ...aggregators,
+    });
+  }
 }
 
 module.exports = Services;

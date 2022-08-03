4c6145382b3836a2882e700b168b241afa239ccd
João Lourenço <joaodslourenco@gmail.com>
2022-08-03 16:08:09 -0300

    refactor: server now runs on .env port or 3333

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./routes");
-
 dotenv.config();
+
+const port = process.env.PORT || 3333;
 const app = express();
 
 routes(app);
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Servidor rodando na porta ${process.env.PORT}`);
 });
